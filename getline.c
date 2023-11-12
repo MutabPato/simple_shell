b10c551a2cff6c83b2a89e38c6252572cc6a03b4
MutabPato <patrickmtbz@gmail.com>
2023-11-12 11:52:57 -0800

    fix

--- getline.c
+++ getline.c
@@ -5,14 +5,15 @@
  * @input: pointer to address of the buffer containing the text
  * @len: size in bytes
  * @file: pointer to a file / stream
+ *
+ * Return: number of characters read.
  */
 
 ssize_t get_line(char **input, size_t *len, FILE *file)
 {
 	static char buffer[1024];
-	static size_t position = 0;
+	static size_t position;
 	static ssize_t char_read;
-
 	int chr;
 	size_t line_size = 0;
 
@@ -23,29 +24,23 @@ ssize_t get_line(char **input, size_t *len, FILE *file)
 		if (*input == NULL)
 			return (-1);
 	}
-
 	while (1)
 	{
 		if (position >= (size_t)char_read)
 		{
 			position = 0;
 			char_read = read(fileno(file), buffer, sizeof(buffer));
-
 			if (char_read <= 0)
 			{
 				if (line_size > 0)
 					return (line_size);
-
 				else
 					return (-1);
 			}
 		}
-
 		chr = buffer[position++];
-
 		if (chr == '\n')
 			return (line_size);
-
 		if (line_size == *len - 1)
 		{
 			*len *= 2;
@@ -54,6 +49,6 @@ ssize_t get_line(char **input, size_t *len, FILE *file)
 				return (-1);
 		}
 		(*input)[line_size++] = (char)chr;
-
 	}
 }
+
--- shell.c
+++ shell.c
@@ -16,6 +16,7 @@ int main(void)
 	while (1)
 	{
 		printf("($): ");
+		fflush(stdout);
 		read = getline(&input, &len, stdin);
 		
 		if (read == -1)
